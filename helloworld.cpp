2d2b9546a11ab8c72e27bf4cbdfecd0ee391b1f8
Liezl <llizardo@byupathway.edu>
2025-07-05 16:36:05 +0800

    edit launch.json, c_cpp_properties and task.json

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main() {
     cout << "Hello, C++ in VS Code!" << endl;
     cout << "I'm Liezl G. Lizardo" << endl;
+    cout << "Looking forward to work with you" << endl;
     return 0;
 }
